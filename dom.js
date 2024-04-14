49dae4f4df01d18281cbbede6ba8d933521e40ea
ToasteRu <voloshkonikita@bk.ru>
2024-04-14 10:06:49 +0300

    HomeWork-3

--- dom.js
+++ dom.js
@@ -1,3 +1,19 @@
-let allArticlesText = document.getElementById("allarticles").textContent;
-allArticlesText = allArticlesText+" (8)";
-console.log(allArticlesText);
+let sectionArticles = document.querySelectorAll(".article")[0];
+let sectionCommunication = document.querySelectorAll(".communication")[0];
+
+console.log(sectionArticles);
+console.log(sectionCommunication);
+
+let goToArticlesBtn = document.getElementById("GoToArticles");
+let goToCommunicationBtn = document.getElementById("GoToCommunication");
+
+goToArticlesBtn.addEventListener("click", () =>{
+    sectionArticles.scrollIntoView({behavior:"smooth"});
+});
+
+goToCommunicationBtn.addEventListener("click", () =>{
+    sectionCommunication.scrollIntoView({behavior:"smooth"});
+});
+
+console.log(goToArticlesBtn);
+console.log(goToCommunicationBtn);
