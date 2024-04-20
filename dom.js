3d114d9e80f51fb6d13e781539b95c7dce53af5b
ToasteRu <voloshkonikita@bk.ru>
2024-04-20 08:52:04 +0300

    hw 3, att.2

--- dom.js
+++ dom.js
@@ -1,19 +1,15 @@
 let sectionArticles = document.querySelectorAll(".article")[0];
 let sectionCommunication = document.querySelectorAll(".communication")[0];
 
-console.log(sectionArticles);
-console.log(sectionCommunication);
-
 let goToArticlesBtn = document.getElementById("GoToArticles");
 let goToCommunicationBtn = document.getElementById("GoToCommunication");
 
-goToArticlesBtn.addEventListener("click", () =>{
-    sectionArticles.scrollIntoView({behavior:"smooth"});
-});
+function scrollToElem (elem){
+    elem.scrollIntoView({behavior:"smooth"});
+};
 
-goToCommunicationBtn.addEventListener("click", () =>{
-    sectionCommunication.scrollIntoView({behavior:"smooth"});
-});
+goToArticlesBtn.addEventListener ("click", scrollToElem (sectionArticles));
+goToCommunicationBtn.addEventListener ("click", scrollToElem (sectionCommunication));
 
 console.log(goToArticlesBtn);
 console.log(goToCommunicationBtn);
